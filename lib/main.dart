5fc3e41583e4e413cc1228a9c0d0bc624872de9c
Julian Scherschmidt <julian@scherschmidt.de>
2026-02-06 21:22:42 +0100

    added login screen with signup / signin / forgot password

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,136 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_login/flutter_login.dart';
 import 'package:wurp/firebase_options.dart';
 import 'package:wurp/ui/scrolling_container.dart';
 
-void main() async{
+FirebaseApp? app;
+FirebaseAuth? auth;
+
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  Firebase.initializeApp(name: "scroller", options: DefaultFirebaseOptions.currentPlatform);
+  app = await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  auth = FirebaseAuth.instanceFor(app: app!);
+
+  FirebaseAuth.instance.authStateChanges().listen((User? user) {
+    if (user != null) {
+      print("User id: ${user.uid}");
+    }
+  });
+
   runApp(const MyApp());
+}
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  Duration get loginTime => const Duration(milliseconds: 2250);
+  bool enteredPasswordIncorrectly = false;
+
+  Future<String?> _authUser(LoginData data) async {
+    print("login data ${data.name}, ${data.password}");
+
+    try {
+      await FirebaseAuth.instance.signInWithEmailAndPassword(email: data.name, password: data.password);
+    } on FirebaseAuthException catch (e) {
+      String? fullMessage = e.message;
+      print("$fullMessage");
+      if (fullMessage?.contains("internal") ?? false) {
+        setState(() {
+          enteredPasswordIncorrectly = true;
+        });
+      }
+      return fullMessage ?? "An unknown error has occurred!";
+    } catch (e) {
+      print("unknown signup error! $e");
+      return "an unknown error has occurred!";
+    }
+  }
+
+  Future<String?> _signupUser(SignupData data) async {
+    print("signup data ${data.name}, ${data.password}, additional: ${data.additionalSignupData}");
+
+    if (data.password == null || data.name == null) return "please enter a valid email or password!";
+
+    try {
+      await FirebaseAuth.instance.createUserWithEmailAndPassword(email: data.name!, password: data.password!);
+    } on FirebaseAuthException catch (e) {
+      String? fullMessage = e.message;
+      print("$fullMessage");
+      return fullMessage ?? "An unknown error has occurred!";
+    } catch (e) {
+      print("unknown signup error! $e");
+      return "an unknown error has occurred!";
+    }
+  }
+
+
+  Future<String?> _recoverPassword(String email) async {
+    try {
+      await FirebaseAuth.instance.sendPasswordResetEmail(
+        email: email.trim(),
+      );
+      print("sent to '$email'");
+      return null;
+    } on FirebaseAuthException catch (e) {
+      return e.message ?? 'Password reset failed';
+    }
+  }
+
+  
+  Future<String?> _sendCode(String email) async{
+    try {
+      await FirebaseAuth.instance.sendPasswordResetEmail(email: email);
+    } on FirebaseAuthException catch(e) {
+      print("Password Recovery error! $e");
+      return e.message;
+    }
+    return null;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FlutterLogin(
+      onLogin: _authUser,
+      onSignup: _signupUser,
+      onRecoverPassword: _recoverPassword,
+      onConfirmRecover: null,
+      onResendCode: null,
+
+      onSubmitAnimationCompleted: () {
+        Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) => const MyHomePage(title: "scroller test")));
+      }
+    );
+  }
+}
+
+class ResetPasswordButton extends StatelessWidget {
+  const ResetPasswordButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () => showMenu(
+        context: context,
+        items: [PopupMenuItem(child: ResetPasswordPopup())],
+        position: RelativeRect.fill
+      ),
+      child: Text("forgot password"),
+    );
+  }
+}
+
+class ResetPasswordPopup extends StatelessWidget {
+  const ResetPasswordPopup({super.key});
+
+  @override
+  Widget build(BuildContext context) => Card(child: Text("send email"), margin: EdgeInsets.all(10),);
 }
 
 class MyApp extends StatelessWidget {
@@ -15,14 +139,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Lerntok halt',
-      theme: ThemeData(
-        colorScheme: .dark(),
-      ),
-      home: const MyHomePage(title: 'Tiktok fürs lernen oder so'),
-      debugShowCheckedModeBanner: false,
-    );
+    return MaterialApp(title: 'Lerntok halt', home: const LoginScreen(), debugShowCheckedModeBanner: false);
   }
 }
 
@@ -54,16 +171,14 @@ class _MyHomePageState extends State<MyHomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
-        appBar: AppBar(
-          // TRY THIS: Try changing the color here to a specific color (to
-          // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-          // change color while the other colors stay the same.
-          backgroundColor: Theme.of(context).colorScheme.primary,
-          title: Text(widget.title),
-        ),
-        body: Center(
-            child: ScrollingContainer()
-        )
+      appBar: AppBar(
+        // TRY THIS: Try changing the color here to a specific color (to
+        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
+        // change color while the other colors stay the same.
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        title: Text(widget.title),
+      ),
+      body: Center(child: ScrollingContainer()),
     );
   }
 }
