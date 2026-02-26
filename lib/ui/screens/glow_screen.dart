f788971ec4ca00555343a7216978deb3f6f45296
Julian Scherschmidt <julian@scherschmidt.de>
2026-02-26 20:31:30 +0100

    updated glowScreen background

--- lib/ui/screens/glow_screen.dart
+++ lib/ui/screens/glow_screen.dart
@@ -1,6 +1,7 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 
 class Glowscreen extends StatefulWidget{
   final Widget child;
@@ -11,42 +12,42 @@ class Glowscreen extends StatefulWidget{
 }
 
 class _GlowscreenState extends State<Glowscreen> {
-
-  final Color _dominantColor = Colors.grey;
+  Color? _dominantColor;
   
   @override
-  Widget build(BuildContext context) => Scaffold(
-    backgroundColor: Colors.black,
-    body: Stack(
-      children: [
-        Positioned.fill(
-          child: AnimatedContainer(
-            duration: const Duration(milliseconds: 800),
-            decoration: BoxDecoration(
-              gradient: RadialGradient(
-                center: Alignment.center,
-                radius: 1.2,
-                colors: [
-                  _dominantColor.withValues(alpha: 0.4),
-                  _dominantColor.withValues(alpha: 0.2),
-                  _dominantColor.withValues(alpha: 0.1),
-                  Colors.transparent,
-                ],
-                stops: const [0.0, 0.3, 0.6, 1.0],
+  Widget build(BuildContext context) {
+    _dominantColor ??= Theme.of(context).colorScheme.surface;
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Stack(
+        children: [
+          Positioned.fill(
+            child: AnimatedContainer(
+              duration: const Duration(milliseconds: 800),
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [
+                    _dominantColor!.withValues(alpha: 1),
+                    _dominantColor!.withValues(alpha: 0.8),
+                    _dominantColor!.withValues(alpha: 0.9),
+                  ],
+                  transform: const GradientRotation(0.7),
+                  stops: const [0.7, 0.6, 1.0],
+                ),
               ),
             ),
           ),
-        ),
-        Positioned.fill(
-          child: BackdropFilter(
-            filter: ImageFilter.blur(sigmaX: 80, sigmaY: 80),
-            child: Container(
-              color: Colors.transparent,
+          Positioned.fill(
+            child: BackdropFilter(
+              filter: ImageFilter.blur(sigmaX: 80, sigmaY: 80),
+              child: Container(
+                color: Colors.transparent,
+              ),
             ),
           ),
-        ),
-        widget.child
-      ],
-    ),
-  );
+          widget.child
+        ],
+      ),
+    );
+  }
 }
