3922b1286418e5b430db20818e5bd97c47b80e34
RiddleNeon <julian@scherschmidt.de>
2026-04-25 22:45:56 +0200

    fixed some more bugs and added thumbnail previews to cover loading indicators

--- lib/ui/misc/shorts_player.dart
+++ lib/ui/misc/shorts_player.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
@@ -20,7 +21,17 @@ class ShortsFeed extends StatefulWidget {
 class _ShortsFeedState extends State<ShortsFeed> {
   final PageController _pageController = PageController();
 
-  final videos = [ShortVideo('YDDHUQYh1yw'), ShortVideo('dQw4w9WgXcQ'), ShortVideo('3JZ_D3ELwOQ')];
+  final videos = [
+    ShortVideo('YDDHUQYh1yw'),
+    ShortVideo('dQw4w9WgXcQ'),
+    ShortVideo('3JZ_D3ELwOQ'),
+    ShortVideo('YDDHUQYh1yw'),
+    ShortVideo('dQw4w9WgXcQ'),
+    ShortVideo('3JZ_D3ELwOQ'),
+    ShortVideo('YDDHUQYh1yw'),
+    ShortVideo('dQw4w9WgXcQ'),
+    ShortVideo('3JZ_D3ELwOQ'),
+  ];
 
   final Map<int, YoutubePlayerController> _controllers = {};
 
@@ -30,11 +41,21 @@ class _ShortsFeedState extends State<ShortsFeed> {
     return YoutubePlayerController.fromVideoId(
       videoId: videoId,
       autoPlay: true,
-      params: const YoutubePlayerParams(showControls: false, showFullscreenButton: false, mute: false, loop: true, playsInline: true, pointerEvents: .none, enableCaption: false),
+      params: const YoutubePlayerParams(
+        showControls: false,
+        showFullscreenButton: false,
+        mute: false,
+        loop: true,
+        playsInline: true,
+        pointerEvents: .none,
+        enableCaption: false,
+      ),
     )..loadVideoById(videoId: videoId);
   }
 
   void _onPageChanged(int index) {
+    if (index == _currentIndex) return;
+
     _controllers[_currentIndex]?.close();
     _controllers.remove(_currentIndex);
 
@@ -68,37 +89,84 @@ class _ShortsFeedState extends State<ShortsFeed> {
       controller: _pageController,
       onPageChanged: _onPageChanged,
       itemCount: videos.length,
+      physics: const PageScrollPhysics(),
       itemBuilder: (context, index) {
         _controllers[index] ??= _createController(videos[index].id);
 
-        return ShortVideoPage(controller: _controllers[index]!);
+        return ShortVideoPage(controller: _controllers[index]!, videoId: videos[index].id);
       },
     );
   }
 }
 
-class ShortVideoPage extends StatelessWidget {
+class ShortVideoPage extends StatefulWidget {
   final YoutubePlayerController controller;
+  final String videoId;
 
-  const ShortVideoPage({super.key, required this.controller});
+  const ShortVideoPage({super.key, required this.controller, required this.videoId});
+
+  @override
+  State<ShortVideoPage> createState() => _ShortVideoPageState();
+}
+
+class _ShortVideoPageState extends State<ShortVideoPage> {
+  bool _startedPlaying = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    widget.controller.listen((event) {
+      if (event.playerState == PlayerState.playing && !_startedPlaying) {
+        setState(() {
+          _startedPlaying = true;
+        });
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Stack(
       children: [
-        Center(child: YoutubePlayer(gestureRecognizers: <Factory<OneSequenceGestureRecognizer>>{}, controller: controller, aspectRatio: 9 / 16)),
-        Center(child: PointerInterceptor(child: const AspectRatio(aspectRatio: 9 / 16))),
+        Center(
+          child: Opacity(
+            opacity: _startedPlaying ? 1.0 : 0.01,
+            child: YoutubePlayer(gestureRecognizers: <Factory<OneSequenceGestureRecognizer>>{}, controller: widget.controller, aspectRatio: 9 / 16),
+          ),
+        ),
+        Positioned.fill(
+          child: Opacity(
+            opacity: _startedPlaying ? 0.0 : 1.0,
+            child: ClipRect(
+              child: FittedBox(
+                fit: BoxFit.cover,
+                alignment: Alignment.center,
+                child: SizedBox(
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.height,
+                  child: CachedNetworkImage(
+                    imageUrl: 'https://img.youtube.com/vi/${widget.videoId}/hqdefault.jpg',
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+        Center(
+          child: PointerInterceptor(child: const AspectRatio(aspectRatio: 9 / 16)),
+        ),
 
         // tap overlay
         Positioned.fill(
           child: GestureDetector(
             onTap: () async {
-              print("tapped video, checking state...");
-              final state = await controller.playerState;
+              final state = await widget.controller.playerState;
               if (state == PlayerState.playing) {
-                controller.pauseVideo();
+                widget.controller.pauseVideo();
               } else {
-                controller.playVideo();
+                widget.controller.playVideo();
               }
             },
           ),
