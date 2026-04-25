48d1513129549cabe7d11d8e67d87f4bf71febdf
RiddleNeon <julian@scherschmidt.de>
2026-04-25 20:28:13 +0200

    fixed an iframe bug when watching a video multiple times

--- lib/ui/misc/shorts_player.dart
+++ lib/ui/misc/shorts_player.dart
@@ -1,187 +1,3 @@
-/*
-import 'dart:async';
-
-import 'package:flutter/material.dart';
-import 'package:webview_flutter/webview_flutter.dart';
-import 'package:wurp/logic/video/video.dart';
-
-class ShortPlayer extends StatefulWidget {
-  final Video? video;
-
-  const ShortPlayer({super.key, this.video});
-
-  @override
-  State<ShortPlayer> createState() => _ShortPlayerState();
-}
-
-class _ShortPlayerState extends State<ShortPlayer> {
-  late final WebViewController _controller;
-
-  final html = """
-<!DOCTYPE html>
-<html>
-  <body style="margin:0">
-    <div id="player"></div>
-
-    <script src="https://www.youtube.com/iframe_api"></script>
-
-    <script>
-      var player;
-      
-      function notifyFlutterReady() {
-        if (typeof window.playerReadyChannel !== "undefined") {
-          console.log("Sending ready to Flutter");
-          window.playerReadyChannel.postMessage("playerReady");
-        } else {
-          console.log("playerReadyChannel not defined, retrying...");
-          setTimeout(notifyFlutterReady, 100);
-        }
-      }
-    
-      function initPlayer() {
-        console.log("Creating player...");
-        player = new YT.Player('player', {
-          videoId: 'YDDHUQYh1yw',
-          events: {            
-            onReady: function () {
-              window.location.href = "ready://player";
-            }
-          },
-          playerVars: {
-            autoplay: 1,
-            controls: 0,
-            playsinline: 1
-          }
-        });
-      }
-    
-      function waitForYT() {
-        if (typeof YT !== "undefined" && YT.Player) {
-          console.log("YT API available");
-          initPlayer();
-        } else {
-          console.log("Waiting for YT...");
-          setTimeout(waitForYT, 100);
-        }
-      }
-    
-      // Script manuell laden
-      var tag = document.createElement('script');
-      tag.src = "https://www.youtube.com/iframe_api";
-      document.body.appendChild(tag);
-    
-      // Start polling
-      waitForYT();
-    
-      function pauseVideo() {
-        console.log("Attempting to pause video...");
-        if (player && player.getPlayerState) {
-          var state = player.getPlayerState();
-          
-          // nur pausieren wenn wirklich playing
-          if (state === YT.PlayerState.PLAYING) {
-            player.pauseVideo();
-            console.log("Paused!");
-          } else {
-            console.log("Not playing yet, can't pause:", state);
-          }
-        } else {
-          console.log("Player not ready, can't pause");
-        }
-      }
-    
-      function playVideo() {
-        if (player) player.playVideo();
-      }
-    </script>
-  </body>
-</html>
-""";
-
-  bool _isReady = false;
-
-  Timer? _pauseTimer;
-  Timer? _playTimer;
-  
-  @override
-  void initState() {
-    super.initState();
-
-    _controller = WebViewController()
-      ..setJavaScriptMode(JavaScriptMode.unrestricted)
-      ..addJavaScriptChannel(
-        'playerReadyChannel',
-        onMessageReceived: (message) {
-          if (message.message == 'playerReady') {
-            setState(() {
-              _isReady = true;
-            });
-          }
-        },
-      );
-    print("Loading HTML into WebView...");
-    _controller.loadHtmlString(html);
-
-    _controller.setNavigationDelegate(
-      NavigationDelegate(
-        onPageFinished: (url) {
-          print("Page loaded");
-        },
-        onWebResourceError: (error) {
-          print("Web error: $error");
-        },
-      ),
-    );
-
-    _playTimer = Timer.periodic(const Duration(seconds: 1), (timer) async {
-      if (_isReady) {
-        _controller.runJavaScript("""
-        player.loadVideoById('${widget.video?.id}');
-      """);
-        timer.cancel();
-      }
-    });
-
-    _pauseTimer = Timer.periodic(const Duration(seconds: 1), (timer) async {
-      if (_isReady) {
-        _controller.runJavaScript("if (typeof player !== 'undefined') pauseVideo();");
-        print("paused video");
-      } else {
-        print("player not ready yet");
-      }
-    });
-  }
-  
-  @override
-  void dispose() {
-    _playTimer?.cancel();
-    _pauseTimer?.cancel();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        WebViewWidget(controller: _controller),
-
-        Center(
-          child: GestureDetector(
-            onTap: () {
-              _controller.runJavaScript("playVideo()");
-            },
-            onDoubleTap: () {
-              _controller.runJavaScript("pauseVideo()");
-            },
-            child: Container(color: Colors.transparent),
-          ),
-        ),
-      ],
-    );
-  }
-}
-*/
-
 import 'package:flutter/foundation.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
@@ -213,20 +29,17 @@ class _ShortsFeedState extends State<ShortsFeed> {
   YoutubePlayerController _createController(String videoId) {
     return YoutubePlayerController.fromVideoId(
       videoId: videoId,
-      autoPlay: false,
-      params: const YoutubePlayerParams(showControls: false, showFullscreenButton: false, mute: true, loop: true, playsInline: true, pointerEvents: .none, enableCaption: false),
+      autoPlay: true,
+      params: const YoutubePlayerParams(showControls: false, showFullscreenButton: false, mute: false, loop: true, playsInline: true, pointerEvents: .none, enableCaption: false),
     )..loadVideoById(videoId: videoId);
   }
 
   void _onPageChanged(int index) {
-    // pause old
     _controllers[_currentIndex]?.close();
+    _controllers.remove(_currentIndex);
 
-    // create controller if needed
     _controllers[index] ??= _createController(videos[index].id);
-
-    // play new
-    //_controllers[index]!.playVideo();
+    _controllers[index]!.playVideo();
 
     _currentIndex = index;
   }
